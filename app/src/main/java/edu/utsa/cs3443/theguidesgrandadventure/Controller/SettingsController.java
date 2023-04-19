f8148848fd61ddf0c9ba246f7a1be4193f348293
jrgg1 <123987252+jrgg1@users.noreply.github.com>
2023-04-19 13:01:02 -0500

    Furnished settings xml, added functionality to return button, also added font/image data for aesthetic use.

--- app/src/main/java/edu/utsa/cs3443/theguidesgrandadventure/Controller/SettingsController.java
+++ app/src/main/java/edu/utsa/cs3443/theguidesgrandadventure/Controller/SettingsController.java
@@ -2,12 +2,24 @@ package edu.utsa.cs3443.theguidesgrandadventure.Controller;
 
 import android.view.View;
 
-//Settings menu controller
+import edu.utsa.cs3443.theguidesgrandadventure.R;
+import edu.utsa.cs3443.theguidesgrandadventure.SettingsActivity;
+
+//Settings Menu Controller
 //Jose
 public class SettingsController implements View.OnClickListener{
 
+    private SettingsActivity activity;
+    public static boolean musicOn = true; // For use later.
+
+    public SettingsController(SettingsActivity activity) {
+        this.activity = activity;
+    }
+
     @Override
     public void onClick(View view) {
-
+        if(view.getId() == R.id.return_button) { //Returns to In-game Menu.
+            activity.finish();
+        }
     }
 }
--- app/src/main/java/edu/utsa/cs3443/theguidesgrandadventure/SettingsActivity.java
+++ app/src/main/java/edu/utsa/cs3443/theguidesgrandadventure/SettingsActivity.java
@@ -1,16 +1,31 @@
 package edu.utsa.cs3443.theguidesgrandadventure;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import edu.utsa.cs3443.theguidesgrandadventure.Controller.SettingsController;
 
 //Settings menu activity
 //Jose - testing my local repo again
 public class SettingsActivity extends AppCompatActivity {
 
+    private SettingsController settingsController;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        settingsController = new SettingsController(this);
+
+        Button returnMenuButton = findViewById(R.id.return_button);
+        setupButton(returnMenuButton);
+    }
+
+    private void setupButton(View view){
+        view.setOnClickListener(settingsController);
     }
 }
+
