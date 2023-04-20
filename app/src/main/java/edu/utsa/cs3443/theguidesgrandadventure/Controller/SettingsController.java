c451a4e211da41ebbccffc0e694a9a03a2647643
jrgg1 <123987252+jrgg1@users.noreply.github.com>
2023-04-20 10:49:08 -0500

    Started Javadoc formatting

--- app/src/main/java/edu/utsa/cs3443/theguidesgrandadventure/Controller/SettingsController.java
+++ app/src/main/java/edu/utsa/cs3443/theguidesgrandadventure/Controller/SettingsController.java
@@ -4,21 +4,37 @@ import android.view.View;
 import edu.utsa.cs3443.theguidesgrandadventure.R;
 import edu.utsa.cs3443.theguidesgrandadventure.SettingsActivity;
 
-//Settings Menu Controller
-//Jose
+/**
+ * The SettingsController class implements the OnClickListener and controls events for
+ * SettingsActivity. The class also holds a class constructor and onClick for button interactions.
+ *
+ * @author Jose Gracia
+ * @version 1.0
+ * @since 2023-04-20
+ */
 public class SettingsController implements View.OnClickListener {
 
-    private SettingsActivity activity;
-    public static boolean musicOn = true; // For use later.
+    private final SettingsActivity activity;
 
+    /**
+     * Constructor method for SettingsController.
+     *
+     * @param activity - This is the activity of SettingsActivity class.
+     */
     public SettingsController(SettingsActivity activity) {
         this.activity = activity;
     }
 
+    /**
+     * onClick method for SettingsController, it controls interactions for the
+     * SettingsActivity.class.
+     *
+     * @param view - This is a view item from the settings xml file.
+     */
     @Override
     public void onClick(View view) {
-        if(view.getId() == R.id.return_button) { //Returns to In-game Menu.
-            activity.finish();
+        if(view.getId() == R.id.return_button) {
+            activity.finish(); // This will end the settings activity.
         }
     }
 }
--- app/src/main/java/edu/utsa/cs3443/theguidesgrandadventure/SettingsActivity.java
+++ app/src/main/java/edu/utsa/cs3443/theguidesgrandadventure/SettingsActivity.java
@@ -12,12 +12,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import edu.utsa.cs3443.theguidesgrandadventure.Controller.SettingsController;
 import edu.utsa.cs3443.theguidesgrandadventure.Model.MediaPlayerManager;
 
-//Settings menu activity
-//Jose - testing my local repo again
+/**
+ * The SettingsActivity class creates a SettingsController and acts as the activity for the
+ * settings view of the app.
+ *
+ * @author Jose Gracia
+ * @version 1.0
+ * @since 2023-04-20
+ */
 public class SettingsActivity extends AppCompatActivity {
     private SettingsController settingsController;
     private MediaPlayerManager mediaPlayerManager;
     private ToggleButton musicToggleButton;
+
+    /**
+     * onCreate method for SettingsActivity that creates a controller, mediaPlayer, and buttons on
+     * startup for use by the program.
+     *
+     * @param savedInstanceState - Instance state of the class.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,6 +56,10 @@ public class SettingsActivity extends AppCompatActivity {
         setupButton(returnMenuButton);
     }
 
+    /**
+     *
+     * @param view This is a view item from the settings xml file, normally a button.
+     */
     private void setupButton(View view){
         view.setOnClickListener(settingsController);
     }
